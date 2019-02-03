6d600713b92e2c5e9bed02b42dbf3025cd3025be
Marcus Beach <marcusleebeach@gmail.com>
2019-02-02 20:21:29 -0500

    Update DataTypes.java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,10 @@
+/** A Class with a few data types  **/
 public class DataTypes {
     public static void main(String[] args) {
+        // Number
         byte myNum = 100;
         System.out.println(myNum);
+        // Short Numeber
         short myShortNum = 5000;
         System.out.println(myShortNum);
     }
