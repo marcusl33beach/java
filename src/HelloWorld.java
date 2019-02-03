ef8bb63dd0f45216da7d54456281682e5fc9b2ce
Marcus Beach <marcusleebeach@gmail.com>
2019-02-02 20:37:01 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
+/** The code below will print the words Hello World
+to the screen, and it is amazing */
 public class HelloWorld {
     public static void main(String[] args) {
-       /* The code below will print the words Hello World
-       to the screen, and it is amazing */
         System.out.println("Hello World");
     }
 }
