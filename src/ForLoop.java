ec7c711ed3c24174d3784527db2aac48c27f7af0
Marcus Beach <marcusleebeach@gmail.com>
2019-02-03 19:37:09 -0500

    Create ForLoop.java

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+/**
+ * A Class to show a for loop. This loop will count to 299
+ */
+public class ForLoop {
+    public static void main(String[] args){
+        for (int count = 0; count < 300; count++) {
+            System.out.println(count);
+        }
+    }
+}
