be0c6ed586c9ec1028856aa395131ee96c50a488
Marcus Beach <marcusleebeach@gmail.com>
2019-02-03 19:08:44 -0500

    Create Whiles.java

---
+++ src/Whiles.java
@@ -0,0 +1,12 @@
+/**
+ * Class to show a while. This while will count to 1000000
+ */
+public class Whiles {
+    public static void main(String[] args){
+        int count = 0;
+        while (count < 1000001){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
