063509ae842ecd33b1cd5f57b1335d5362953543
Marcus Beach <marcusleebeach@gmail.com>
2019-02-03 19:16:03 -0500

    Create DoWhiles.java

---
+++ src/DoWhiles.java
@@ -0,0 +1,13 @@
+/**
+ * Class to show a do while loop. This will count to 2000000
+ */
+public class DoWhiles {
+    public static void main(String[] args){
+        int count = 0;
+        do{
+            System.out.println(count);
+            count++;
+        }
+        while (count < 2000001);
+    }
+}
