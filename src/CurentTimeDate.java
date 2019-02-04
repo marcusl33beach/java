43966692a4b8505f484ac7b7a85bc341f239fade
Marcus Beach <marcusleebeach@gmail.com>
2019-02-03 19:25:58 -0500

    Create CurentTimeDate.java

---
+++ src/CurentTimeDate.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+/**
+ * A Class to print the current time and date
+ */
+public class CurentTimeDate {
+    public static void main(String[] args){
+        System.out.println(LocalDateTime.now());
+    }
+}
