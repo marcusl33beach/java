b28e0ea9459e1cb1ae5a65ed505bd9af7a74c972
Marcus Beach <marcusleebeach@gmail.com>
2019-02-05 18:27:52 -0500

    Update Number.java

--- src/Number.java
+++ src/Number.java
@@ -1,5 +1,5 @@
 /** Just adding a few variables together in a class */
-public class Numbers {
+public class Number {
     public static void main(String[] args){
         int x = 5, y = 6, z = 50;
         System.out.println(x + y + z);
