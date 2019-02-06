06b2eb85722e0c0b5853066455249ed82deb06c9
Marcus Beach <marcusleebeach@gmail.com>
2019-02-05 19:43:10 -0500

    Create Boolean.java

---
+++ src/Boolean.java
@@ -0,0 +1,17 @@
+/**
+ * Class to show some Boolean variables
+ */
+public class Boolean {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 9;
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+
+        System.out.print(isJavaFun); // Should print true
+        System.out.print(isFishTasty); // Should print false
+        System.out.print(x > y); // returns true
+        System.out.print( x == 10); // returns true
+        System.out.print( 10 == 15); // returns false
+    }
+}
