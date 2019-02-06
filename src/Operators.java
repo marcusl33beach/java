22639b7a7a828f2ae645404d0dae494f0fcefdfd
Marcus Beach <marcusleebeach@gmail.com>
2019-02-05 19:30:16 -0500

    Create Operators.java

---
+++ src/Operators.java
@@ -0,0 +1,11 @@
+/**
+ * Class to show a few operators
+ */
+public class Operators {
+    public static void main(String[] args){
+        int sum1 = 100 + 50; // Adding 100 and 50 to get 150
+        int sum2 = sum1 + 250; // Adding sum1 to 250 to get 400
+        int sum3 = sum2 + sum2; // Adding sum2 and sum2 to get 800
+        System.out.println(sum3); // Prints out sum3
+    }
+} // returns 800
