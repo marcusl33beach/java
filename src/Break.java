409b23f74a5a08a21ce2b8366df4dd326b9e22d8
Marcus Beach <marcusleebeach@gmail.com>
2019-02-05 19:58:16 -0500

    Create Break.java

---
+++ src/Break.java
@@ -0,0 +1,15 @@
+/**
+ * Class to count to 3 stop before 4
+ */
+public class Break {
+    public static void main(String[] args){
+        // For loop
+        for (int i = 0; i < 10; i++){
+            // If with break to make the loop stop at 3
+            if (i == 4){
+                break;
+            }
+            System.out.print(i); // Print out tht numbers 
+        }
+    }
+}
