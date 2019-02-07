8c22703e2c8e2afc8745a09fbf09b940ed8f1aa5
Marcus Beach <marcusleebeach@gmail.com>
2019-02-06 20:24:27 -0500

    Create ClassObjects.java

---
+++ src/ClassObjects.java
@@ -0,0 +1,11 @@
+/**
+ * A Class to show a basic class object
+ */
+public class ClassObjects {
+    int x = 5;
+
+    public static void main(String[] args){
+        ClassObjects myObj = new ClassObjects();
+        System.out.println(myObj.x);
+    }
+} // Output 5 
