2a5089011c7daeef7083342f20ba9354297fc313
Marcus Beach <marcusleebeach@gmail.com>
2019-02-11 20:39:05 -0500

    Create ClassMethod.java

---
+++ src/ClassMethod.java
@@ -0,0 +1,22 @@
+/**
+ * Class to show static and public methods
+ */
+public class ClassMethod {
+    // Static method
+    static void staticMethod() {
+       System.out.println(" No need to create a object when calling a static method");
+    }
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("You need to create an object to call a public method");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        staticMethod(); // Calling the static method
+
+        ClassMethod obj = new ClassMethod(); // Making an object to be used below
+        obj.publicMethod(); // Calling the public method with the object.
+    }
+}
